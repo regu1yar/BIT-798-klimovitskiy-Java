bc08c2ce94edb2112005d008aefd85245c2983bd
Roman <roma_klimovitskiy@mail.ru>
2020-05-07 15:23:01 +0300

    Fixed Task logic

--- src/main/java/hw3/task3/task/Task.java
+++ src/main/java/hw3/task3/task/Task.java
@@ -4,54 +4,33 @@ import java.util.concurrent.Callable;
 
 public class Task<T> {
     private final Callable<? extends T> callable;
-    private volatile TaskException thrownException = null;
-    private volatile T result = null;
-    private volatile Boolean called = false;
+    private TaskException thrownException = null;
+    private T result = null;
+    private volatile boolean finished = false;
 
     public Task(Callable<? extends T> callable) {
         this.callable = callable;
     }
 
     public T get() throws TaskException {
+        if (!finished) {
+            synchronized (this) {
+                if (!finished) {
+                    try {
+                        result = callable.call();
+                    } catch (Exception e) {
+                        thrownException = new TaskException(e);
+                    } finally {
+                        finished = true;
+                    }
+                }
+            }
+        }
+
         if (thrownException != null) {
             throw thrownException;
         }
 
-        if (result != null) {
-            return result;
-        }
-
-        if (called) {
-            synchronized (this) {
-                while (result == null && thrownException == null) {
-                    try {
-                        this.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            if (result != null) {
-                return result;
-            } else {
-                throw thrownException;
-            }
-        } else {
-            called = true;
-            try {
-                result = callable.call();
-                synchronized (this) {
-                    this.notifyAll();
-                }
-                return result;
-            } catch (Exception e) {
-                thrownException = new TaskException(e);
-                synchronized (this) {
-                    this.notifyAll();
-                }
-                throw thrownException;
-            }
-        }
+        return result;
     }
 }
